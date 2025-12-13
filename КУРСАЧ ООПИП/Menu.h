c401598b69c200f4bd79a16fdeb475a37cea544e
NishLi1 <nikolastop4@gmail.com>
2025-12-13 22:07:19 +0300

    Delete КУРСАЧ ООПИП directory

--- КУРСАЧ ООПИП/Menu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Table.h"
-#include <vector>
-#include <string>
-#include <conio.h>
-#include <Windows.h>
-
-int menu(const std::vector<std::string>& items, const std::string& title = "");
